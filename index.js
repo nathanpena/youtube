ec0decc64566d1d23b4c6032c33ab1a07fc698d6
Nathan <nnpena@outlook.com>
2019-10-08 22:55:04 -0500

    get playlists

--- index.js
+++ index.js
@@ -9,18 +9,16 @@ const authorizeButton = document.getElementById('authorize-button');
 const signoutButton = document.getElementById('signout-button');
 const content = document.getElementById('content');
 const channelForm = document.getElementById('channel-form');
-const channelInput = document.getElementById('channel-input');
+const playlistInput = document.getElementById('playlist-input');
 const videoContainer = document.getElementById('video-container');
 
-const defaultChannel = 'OsU5UcOn';
-
-// Form submit and change channel
+// Form submit and change playlist
 channelForm.addEventListener('submit', e => {
     e.preventDefault();
 
-    const channel = channelInput.value;
+    const playlistId = playlistInput.value;
 
-    getChannel(channel);
+    requestVideoPlaylist(playlistId);
 });
 
 // Load auth2 library
@@ -53,7 +51,7 @@ function updateSigninStatus(isSignedIn) {
         signoutButton.style.display = 'block';
         content.style.display = 'block';
         videoContainer.style.display = 'block';
-        getChannel(defaultChannel);
+        requestVideoPlaylist('PLKj5UM51wKXgCIVHmOmV-uCiwyTi0Pq_d');
     } else {
         authorizeButton.style.display = 'block';
         signoutButton.style.display = 'none';
@@ -72,51 +70,6 @@ function handleSignoutClick() {
     gapi.auth2.getAuthInstance().signOut();
 }
 
-// Display channel data
-function showChannelData(data) {
-    const channelData = document.getElementById('channel-data');
-    channelData.innerHTML = data;
-}
-
-// Get channel from API
-function getChannel(channel) {
-    gapi.client.youtube.channels
-        .list({
-            part: 'snippet,contentDetails,statistics',
-            forUsername: channel
-        })
-        .then(response => {
-            console.log(response);
-            const channel = response.result.items[0];
-
-            const output = `
-        <ul class="collection">
-          <li class="collection-item">Title: ${channel.snippet.title}</li>
-          <li class="collection-item">ID: ${channel.id}</li>
-          <li class="collection-item">Subscribers: ${numberWithCommas(
-                    channel.statistics.subscriberCount
-                )}</li>
-          <li class="collection-item">Views: ${numberWithCommas(
-                    channel.statistics.viewCount
-                )}</li>
-          <li class="collection-item">Videos: ${numberWithCommas(
-                    channel.statistics.videoCount
-                )}</li>
-        </ul>
-        <p>${channel.snippet.description}</p>
-        <hr>
-        <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${
-                channel.snippet.customUrl
-                }">Visit Channel</a>
-      `;
-            showChannelData(output);
-
-            const playlistId = channel.contentDetails.relatedPlaylists.uploads;
-            requestVideoPlaylist(playlistId);
-        })
-        .catch(err => alert('No Channel By That Name'));
-}
-
 // Add commas to number
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
@@ -126,7 +79,7 @@ function requestVideoPlaylist(playlistId) {
     const requestOptions = {
         playlistId: playlistId,
         part: 'snippet',
-        maxResults: 10
+        maxResults: 3
     };
 
     const request = gapi.client.youtube.playlistItems.list(requestOptions);
